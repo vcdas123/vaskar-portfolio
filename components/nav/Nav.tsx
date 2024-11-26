ba1fd37c779c125531c51d1732581b0d388886db
Vaskar <143987997+vaskardas123@users.noreply.github.com>
2024-11-26 15:51:47 +0530

    completed skills section

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -1,43 +1,44 @@
-"use client";
-import React, { useState } from "react";
-import { Button } from "../ui/button";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "../ui/sheet";
-import { CiMenuFries } from "react-icons/ci";
+'use client';
+import React, { ReactNode, useState } from 'react';
+import { Button } from '../ui/button';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { Sheet, SheetContent, SheetTrigger } from '../ui/sheet';
+import { CiMenuFries } from 'react-icons/ci';
+import { FaAddressCard, FaHome, FaServicestack } from 'react-icons/fa';
+import { IoMdContact } from 'react-icons/io';
+import { GrProjects } from 'react-icons/gr';
 
 interface LinksIn {
   name: string;
   path: string;
+  icon: ReactNode;
 }
 
 const LINKS_DATA: LinksIn[] = [
   {
-    name: "home",
-    path: "/",
+    name: 'home',
+    path: '/',
+    icon: <FaHome />,
   },
   {
-    name: "services",
-    path: "/services",
+    name: 'services',
+    path: '/services',
+    icon: <FaServicestack />,
   },
   {
-    name: "resume",
-    path: "/resume",
+    name: 'resume',
+    path: '/resume',
+    icon: <FaAddressCard />,
   },
   // {
   //   name: "work",
   //   path: "/work",
   // },
   {
-    name: "contact",
-    path: "/contact",
+    name: 'contact',
+    path: '/contact',
+    icon: <IoMdContact />,
   },
 ];
 
@@ -45,7 +46,7 @@ const Nav = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const pathname = usePathname();
 
-  const handleOpen = () => setIsOpen(true);
+  // const handleOpen = () => setIsOpen(true);
   const handleClose = () => setIsOpen(false);
 
   const links = (
@@ -55,11 +56,12 @@ const Nav = () => {
           <Link
             key={link?.name}
             href={link?.path}
-            className={`capitalize transition-all hover:text-accent-hover font-medium ${
-              link.path === pathname && "text-accent border-b-2 border-accent"
+            className={`capitalize transition-all flex items-center gap-1 hover:text-accent-hover font-medium ${
+              link.path === pathname && 'text-accent border-b-2 border-accent'
             }`}
             onClick={handleClose}
           >
+            {link?.icon}
             {link?.name}
           </Link>
         );
@@ -71,7 +73,9 @@ const Nav = () => {
     <div className="hidden xl:flex items-center gap-8">
       <nav className="flex gap-8">{links}</nav>
       <Link href="/projects">
-        <Button>Projects</Button>
+        <Button className="flex gap-1">
+          <GrProjects /> Projects
+        </Button>
       </Link>
     </div>
   );
@@ -84,7 +88,7 @@ const Nav = () => {
         </SheetTrigger>
         <SheetContent>
           <div className="mt-32 mb-40 text-center text-2xl">
-            <Link href={"/"} onClick={handleClose}>
+            <Link href={'/'} onClick={handleClose}>
               <h1 className="text-4xl font-semibold">
                 Vaskar<span className="text-accent">.</span>
               </h1>
@@ -92,6 +96,15 @@ const Nav = () => {
           </div>
           <nav className="flex flex-col gap-4 justify-center items-center">
             {links}
+            <Link
+              href={'/projects'}
+              className={`capitalize transition-all flex items-center gap-1 hover:text-accent-hover font-medium ${
+                '/projects' === pathname && 'text-accent border-b-2 border-accent'
+              }`}
+              onClick={handleClose}
+            >
+              <GrProjects className="text-sm" /> Projects
+            </Link>
           </nav>
         </SheetContent>
       </Sheet>
