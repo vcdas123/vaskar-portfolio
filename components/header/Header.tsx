7ad8cd808e5e14b64af1ca842d5a201f5068abd8
vcdas123 <vcdas123@gmail.com>
2024-11-30 13:05:22 +0530

    updated the cv

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,14 +1,14 @@
-'use client';
-import Link from 'next/link';
-import React from 'react';
-import Nav from '../nav/Nav';
+"use client";
+import Link from "next/link";
+import React from "react";
+import Nav from "../nav/Nav";
 
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
       <div className="container mx-auto flex justify-between items-center ">
         {/* LOGO */}
-        <Link href={'/'}>
+        <Link href={"/"}>
           <h1 className="text-4xl font-semibold">
             Vaskar<span className="text-accent">.</span>
           </h1>
