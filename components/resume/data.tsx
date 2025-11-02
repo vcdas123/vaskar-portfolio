1e2060b942a676c02f150a13b2302b80c7ee4331
vcdas123 <vcdas123@gmail.com>
2025-11-02 13:31:29 +0530

    back to original layout

--- components/resume/data.tsx
+++ components/resume/data.tsx
@@ -1,7 +1,7 @@
-import { FaBootstrap, FaCss3, FaHtml5, FaJs, FaNodeJs } from 'react-icons/fa';
-import { SiExpress, SiMantine, SiTypescript } from 'react-icons/si';
-import { RiNextjsFill, RiReactjsFill, RiTailwindCssFill } from 'react-icons/ri';
-import { DiMongodb, DiMysql } from 'react-icons/di';
+import { FaBootstrap, FaCss3, FaHtml5, FaJs, FaNodeJs } from "react-icons/fa";
+import { SiExpress, SiMantine, SiTypescript } from "react-icons/si";
+import { RiNextjsFill, RiReactjsFill, RiTailwindCssFill } from "react-icons/ri";
+import { DiMongodb, DiMysql } from "react-icons/di";
 
 // ABOUT_DATA
 export interface AboutInfo {
@@ -56,7 +56,7 @@ export interface SkillsData {
 }
 
 function calculateDuration(): string {
-  const startDate: Date = new Date('2023-04-17');
+  const startDate: Date = new Date("2023-04-17");
   const currentDate: Date = new Date();
 
   let years: number = currentDate.getFullYear() - startDate.getFullYear();
@@ -67,156 +67,162 @@ function calculateDuration(): string {
     months += 12;
   }
 
-  const pluralize = (count: number, label: string): string => `${count} ${label}${count !== 1 ? 's' : ''}`;
+  const pluralize = (count: number, label: string): string =>
+    `${count} ${label}${count !== 1 ? "s" : ""}`;
 
-  const yearsText: string = pluralize(years, 'year');
-  const monthsText: string = pluralize(months, 'month');
+  const yearsText: string = pluralize(years, "year");
+  const monthsText: string = pluralize(months, "month");
 
   return `${yearsText} ${monthsText}`;
 }
 
 export const ABOUT_DATA: AboutData = {
-  title: 'About Me',
-  desc: 'As a Software Development Engineer (SDE), I specialize in building robust applications using technologies such as HTML, CSS, JavaScript, TypeScript, React.js, Next.js, Node.js, MongoDB, and MySQL.',
+  title: "About Me",
+  desc: "As a Software Development Engineer (SDE), I specialize in building robust applications using technologies such as HTML, CSS, JavaScript, TypeScript, React.js, Next.js, Node.js, MongoDB, and MySQL.",
   info: [
     {
-      label: 'Name',
-      value: 'Vaskar Chandra Das',
+      label: "Name",
+      value: "Vaskar Chandra Das",
     },
     {
-      label: 'Mobile',
-      value: '(+91) 8910517164',
+      label: "Mobile",
+      value: "(+91) 8910517164",
     },
     {
-      label: 'Email',
-      value: 'vcdas123@gmail.com',
+      label: "Email",
+      value: "vcdas123@gmail.com",
     },
     {
-      label: 'Experience',
+      label: "Experience",
       value: calculateDuration(),
     },
     {
-      label: 'Nationality',
-      value: 'Indian',
+      label: "Nationality",
+      value: "Indian",
     },
     {
-      label: 'Languages',
-      value: 'English, Hindi, Bengali',
+      label: "Languages",
+      value: "English, Hindi, Bengali",
     },
     {
-      label: 'Freelance',
-      value: 'Available',
+      label: "Freelance",
+      value: "Available",
     },
     {
-      label: 'Date of birth',
-      value: '19 July 1997',
+      label: "Date of birth",
+      value: "19 July 1997",
     },
   ],
 };
 
 export const EXPERIENCE_DATA: ExperienceData = {
-  icon: '/public/resume/badge.svg',
-  title: 'Experience',
-  desc: 'Experienced SDE proficient in MERN stack, building scalable web applications with React.js, Node.js, TypeScript, MongoDB, and MySQL.',
+  icon: "/public/resume/badge.svg",
+  title: "Experience",
+  desc: "Experienced SDE proficient in MERN stack, building scalable web applications with React.js, Node.js, TypeScript, MongoDB, and MySQL.",
   companies: [
     {
-      name: 'iRM Cloud',
-      position: 'SDE II',
-      duration: 'Sep 2023 - Present',
+      name: "iRM Cloud",
+      position: "Software Development Engineer II",
+      duration: "Sep 2023 - Present",
     },
-    {
-      name: 'Webhibe Technologies Pvt. Ltd.',
-      position: 'React.js Developer',
-      duration: 'Apr 2023 - Aug 2023',
-    },
+    // {
+    //   name: 'Webhibe Technologies Pvt. Ltd.',
+    //   position: 'React.js Developer',
+    //   duration: 'Apr 2023 - Aug 2023',
+    // },
   ],
 };
 
 export const EDUCATION_DATA: EducationData = {
-  icon: '/public/resume/cap.svg',
-  title: 'Education',
+  icon: "/public/resume/cap.svg",
+  title: "Education",
   desc: "I completed my 10th and 12th from CBSE and earned a Master's in Computer Applications, building a strong foundation in computer science and application development.",
   institutes: [
     {
-      institute: 'Swami Vivekananda University | Barrackpore ',
-      degree: 'Master of Computer Applications',
-      duration: '2022 - 2024',
+      institute: "Swami Vivekananda University | Barrackpore ",
+      degree: "Master of Computer Applications",
+      duration: "2022 - 2024",
     },
     {
-      institute: 'University of Kalyani | Kalyani',
-      degree: 'Bachelor of Science in Chemistry',
-      duration: '2016 - 2019',
+      institute: "University of Kalyani | Kalyani",
+      degree: "Bachelor of Science in Chemistry",
+      duration: "2016 - 2019",
     },
     {
-      institute: 'Kendriya Vidyalaya No.2 | Kanchrapara',
-      degree: 'Higher Secondary | 12th',
-      duration: '2015 - 2016',
+      institute: "Kendriya Vidyalaya No.2 | Kanchrapara",
+      degree: "Higher Secondary | 12th",
+      duration: "2015 - 2016",
     },
     {
-      institute: 'Kendriya Vidyalaya No.2 | Kanchrapara',
-      degree: 'Matriculation | 10th',
-      duration: '2014 - 2015',
+      institute: "Kendriya Vidyalaya No.2 | Kanchrapara",
+      degree: "Matriculation | 10th",
+      duration: "2014 - 2015",
     },
   ],
 };
 
 export const SKILLS_DATA: SkillsData = {
-  title: 'Skills',
-  desc: 'Focused on creating scalable web solutions with MERN stack technologies like React.js, Node.js, TypeScript, MongoDB, and MySQL',
+  title: "Skills",
+  desc: "Focused on creating scalable web solutions with MERN stack technologies like React.js, Node.js, TypeScript, MongoDB, and MySQL",
   skills: [
     {
-      label: 'HTML5',
+      label: "HTML5",
       icon: <FaHtml5 />,
     },
     {
-      label: 'CSS3',
+      label: "CSS3",
       icon: <FaCss3 />,
     },
     {
-      label: 'Tailwind Css',
+      label: "Tailwind Css",
       icon: <RiTailwindCssFill />,
     },
     {
-      label: 'Bootstrap',
+      label: "Bootstrap",
       icon: <FaBootstrap />,
     },
     {
-      label: 'Mantine UI',
+      label: "Mantine UI",
       icon: <SiMantine />,
     },
     {
-      label: 'Javascript',
+      label: "Javascript",
       icon: <FaJs />,
     },
     {
-      label: 'Typescript',
+      label: "Typescript",
       icon: <SiTypescript />,
     },
     {
-      label: 'React.js',
+      label: "React.js",
       icon: <RiReactjsFill />,
     },
     {
-      label: 'Next.js',
+      label: "Next.js",
       icon: <RiNextjsFill />,
     },
     {
-      label: 'Node.js',
+      label: "Node.js",
       icon: <FaNodeJs />,
     },
     {
-      label: 'Express.js',
+      label: "Express.js",
       icon: <SiExpress />,
     },
     {
-      label: 'MongoDB',
+      label: "MongoDB",
       icon: <DiMongodb />,
     },
     {
-      label: 'MySQL',
+      label: "MySQL",
       icon: <DiMysql />,
     },
   ],
 };
 
-export const RESUME_DATA = [EXPERIENCE_DATA, EDUCATION_DATA, SKILLS_DATA, ABOUT_DATA];
+export const RESUME_DATA = [
+  EXPERIENCE_DATA,
+  EDUCATION_DATA,
+  SKILLS_DATA,
+  ABOUT_DATA,
+];
